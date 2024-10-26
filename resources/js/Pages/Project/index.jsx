3802e8a1b9185f008f6f172beeab9a8740f2f345
Nafeel SM <nafeelsm2018@gmail.com>
2024-10-26 22:05:12 +0530

    project-controller

--- resources/js/Pages/Project/index.jsx
+++ resources/js/Pages/Project/index.jsx
@@ -1,31 +1,31 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
 
 // react functional component
-export default function Index(auth, projects) {
+export default function Index({auth, projects}) {
   return (
     <AuthenticatedLayout
       user={auth.user}
       header={
-        <h2 className="text-xl font-semibold leading-tight text-gray-800
-          dark:text-gray-200">
+        <h2
+          className="text-xl font-semibold leading-tight text-gray-800
+          dark:text-gray-200"
+        >
           Projects
         </h2>
       }
-
     >
-      <head title="Projects" />
-      
-       <div className="py-12">
-                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
-                        <div className="p-6 text-gray-900 dark:text-gray-100">
-                            All Projects
-                        </div>
-                    </div>
-                </div>
+      <Head title="Projects" />
+
+      <div className="py-12">
+        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div className="p-6 text-gray-900 dark:text-gray-100">
+              <pre>{JSON.stringify(projects, undefined, 2)}</pre>
             </div>
-
-
+          </div>
+        </div>
+      </div>
     </AuthenticatedLayout>
   );
 }
